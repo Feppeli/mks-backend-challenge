619d7c7f6aa0f02283828e57d20cffee4696b522
Felipe <dfsilvat@gmail.com>
2024-04-25 14:22:00 -0300

    add: swagger and build:crudo-archives

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configurantion
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Api de Filmes')
+    .setDescription('Api contruida com o intuito de manipular BD com filmes')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
